9745ce7eaa83d36cc5b0d802273d72ea1c706ab0
Rafael Bautista Jr <rafael@outliant.com>
2024-02-23 23:28:54 +0800

    - fix rate

--- dist/financialCalculator.js
+++ dist/financialCalculator.js
@@ -1,63 +1,81 @@
 document.querySelectorAll(".payment_item").forEach((item) => {
-    item.addEventListener("click", function () {
-        var dataTerms = this.getAttribute("data-terms");
-        calculateLoan(dataTerms);
-    });
+  item.addEventListener("click", function () {
+    var dataTerms = this.getAttribute("data-terms");
+    calculateLoan(dataTerms);
+  });
 });
 
 function calculateMonthlyPayment(loanAmount, annualRate, termMonths) {
-    const monthlyRate = annualRate / 100 / 12;
-    const discountFactor = (Math.pow(1 + monthlyRate, termMonths) - 1) / (monthlyRate * Math.pow(1 + monthlyRate, termMonths));
-    return loanAmount / discountFactor;
+  const monthlyRate = annualRate / 100 / 12;
+  const discountFactor =
+    (Math.pow(1 + monthlyRate, termMonths) - 1) /
+    (monthlyRate * Math.pow(1 + monthlyRate, termMonths));
+  return loanAmount / discountFactor;
 }
 
 function calculateLoan(termLength) {
-    const totalAmountFinancedElement = document.getElementById('amount_financed');
-    const loanPrice = parseFloat(totalAmountFinancedElement.textContent.replace(/[$,]/g, '')); 
+  const totalAmountFinancedElement = document.getElementById("amount_financed");
+  const loanPrice = parseFloat(
+    totalAmountFinancedElement.textContent.replace(/[$,]/g, "")
+  );
 
-    const interestRateStr = document.getElementById("interest_rate").textContent.replace(/%/g, '');
-    const interestRate = parseFloat(interestRateStr);
+  const interestRateStr = document
+    .getElementById("interest_rate")
+    .textContent.replace(/%/g, "");
+  const interestRate = parseFloat(interestRateStr);
 
-    if (isNaN(loanPrice)) {
-        console.log('Please enter a valid loan amount.');
-        return;
-    }
-    const monthlyPayment = calculateMonthlyPayment(loanPrice, 7.49, termLength);
-    const formattedMonthlyPayment = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    }).format(monthlyPayment);
+  if (isNaN(loanPrice)) {
+    console.log("Please enter a valid loan amount.");
+    return;
+  }
+  const monthlyPayment = calculateMonthlyPayment(loanPrice, 7.49, termLength);
+  const formattedMonthlyPayment = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(monthlyPayment);
 
-    document.querySelector('.price_text').textContent = formattedMonthlyPayment; 
+  document.querySelector(".price_text").textContent = formattedMonthlyPayment;
 }
 
-document.getElementById('loanPrice').addEventListener('input', function () {
-    const selectedTerm = document.querySelector('.payment_item.payment_item_active').getAttribute('data-terms');
-    calculateLoan(selectedTerm);
+document.getElementById("loanPrice").addEventListener("input", function () {
+  const selectedTerm = document
+    .querySelector(".payment_item.payment_item_active")
+    .getAttribute("data-terms");
+  calculateLoan(selectedTerm);
 });
 
 function calculateDownPaymentPercentage(estimatedPrice, downPayment) {
-    var inputToUpdate = document.querySelector(".down_payment--text");
-    const percentageDown = (estimatedPrice / downPayment) * 100;
-    inputToUpdate.textContent =  percentageDown.toFixed(2); + "%";
+  var inputToUpdate = document.querySelector(".down_payment--text");
+  const percentageDown = (estimatedPrice / downPayment) * 100;
+  inputToUpdate.textContent = percentageDown.toFixed(2) + "%";
 }
 
-    document.getElementById('estimate_payment_link').addEventListener("click", function () {
-        const firstPaymentItem = document.querySelector(".payment_item");
-        if (firstPaymentItem) {
-            firstPaymentItem.click();
-        }
-        document.getElementById('loanPrice').value = `$10,000`
-        const estimate_price = parseFloat(document.querySelector("#estimate_price").textContent.replace(/[$,-]/g, '')); 
-        const down_payment = parseFloat(document.querySelector("#down_payment").textContent.replace(/[$,-]/g, ''));
-        const deposit = parseFloat(document.querySelector("#deposit").textContent.replace(/[$,-]/g, ''));
+document
+  .getElementById("estimate_payment_link")
+  .addEventListener("click", function () {
+    const firstPaymentItem = document.querySelector(".payment_item");
+    if (firstPaymentItem) {
+      firstPaymentItem.click();
+    }
+    document.getElementById("loanPrice").value = `$10,000`;
+    const estimate_price = parseFloat(
+      document
+        .querySelector("#estimate_price")
+        .textContent.replace(/[$,-]/g, "")
+    );
+    const down_payment = parseFloat(
+      document.querySelector("#down_payment").textContent.replace(/[$,-]/g, "")
+    );
+    const deposit = parseFloat(
+      document.querySelector("#deposit").textContent.replace(/[$,-]/g, "")
+    );
 
-        const totalAmountFinance = estimate_price - down_payment - deposit;
-        const formattedTotalAmountFinance = new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        }).format(totalAmountFinance);
-        document.getElementById('amount_financed').textContent = formattedTotalAmountFinance;
-        calculateDownPaymentPercentage(estimate_price, down_payment);
-
-    });
+    const totalAmountFinance = estimate_price - down_payment - deposit;
+    const formattedTotalAmountFinance = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(totalAmountFinance);
+    document.getElementById("amount_financed").textContent =
+      formattedTotalAmountFinance;
+    calculateDownPaymentPercentage(estimate_price, down_payment);
+  });
